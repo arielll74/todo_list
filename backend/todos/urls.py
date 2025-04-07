27863f51ff7d82e8cc4e28fcf8ac2a904609cd8a
arielll74 <your-email@example.com>
2025-04-07 23:36:11 +0800

    UPDATED URLS

--- backend/todos/urls.py
+++ backend/todos/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.todo_list, name='todo_list'), 
+    path('', views.TodoViewSet, name='TodoViewSet'), 
     path('<int:pk>/', views.todo_detail, name='todo_detail'), 
 ]
