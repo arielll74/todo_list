7cce1628f699201eeea0a21478f45dff894d01a9
arielll74 <your-email@example.com>
2025-04-07 23:52:45 +0800

    Update CORS_ALLOWED_ORIGINS in settings.py to allow Vercel frontend

--- backend/todobackend/settings.py
+++ backend/todobackend/settings.py
@@ -42,7 +42,11 @@ INSTALLED_APPS = [
      'corsheaders',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "todo-fronted-git-master-arielll74s-projects.vercel.app",
+    "todo-fronted-akl9209r6-arielll74s-projects.vercel.app",
+   
+]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
--- backend/todos/views.py
+++ backend/todos/views.py
@@ -5,3 +5,5 @@ from .serializers import TodoSerializer
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
+    
